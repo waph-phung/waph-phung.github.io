caa9cf15b363722f0dd2f8bdc9c9514f5db062b4
Phu Phung <154654461+phungph-uc@users.noreply.github.com>
2025-05-20 06:25:09 -0400

    Update remote.js

--- remote.js
+++ remote.js
@@ -1,2 +1,2 @@
-document.write('Hello, I come from https://waph-uc.github.io/, but I can get your credentials at ' + 
+document.write('Hello, I come from https://waph-phung.github.io/, but I can get your credentials at ' + 
                location.origin + ', e.g., your cookie: '+ document.cookie);
